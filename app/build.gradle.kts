5d4210ebff747e503140942e15feb425fe1de3b8
friend5hip <jwy97@naver.com>
2024-12-04 12:14:58 +0900

    feat: 로그인 레이아웃 완료 및 회원가입 레이아웃 작업중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,16 +33,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
